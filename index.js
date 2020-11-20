29d3813d1384113a86e37316b6ea347e5867cc85
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-20 17:38:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,5 +23,11 @@ const Calculator = {
   },
   subtract: function(a,b){
     return a - b;
+  },
+  multiply: function(a,b){
+    return a * b;
+  },
+  divide: function(a,b){
+    return a / b;
   }
 }}
